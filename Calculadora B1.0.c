123aaafcd00c00e126292a540ac61fe63fcf2223
mariogpalencia <42212461+mariogpalencia@users.noreply.github.com>
2018-09-12 22:59:43 -0500

    Update Calculadora B1.0.c

--- Calculadora B1.0.c
+++ Calculadora B1.0.c
@@ -5,7 +5,7 @@ float a,b;
      printf("Inserte los numeros \n");
      scanf("%f %f",&a,&b);
      printf("La suma de %.2f y %.2f es %.2f \n",a,b,a+b);
-     printf("La resta de %.2f menos %.2f es %.2f \n",a,b,a+b);
+     printf("La resta de %.2f menos %.2f es %.2f \n",a,b,a-b);
      printf("La multiplicacion de %.2f y %.2f es %.2f \n",a,b,a*b);
      printf("La division de %.2f por %.2f es %.2f \n",a,b,a/b);
      return 0;
